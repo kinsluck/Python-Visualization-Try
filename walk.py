75c5dbd96da2f62a48db815272a2c1b42f3fcee5
kinsluck <kins36936@gmail.com>
2018-05-07 17:55:48 +0800

    Update

--- walk.py
+++ walk.py
@@ -5,11 +5,15 @@ import numpy as np
 from randomwalk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(5000)
     rw.fill_walk()
+    plt.figure(figsize=(10,6))
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
                 s=15, cmap=plt.cm.Blues, edgecolors='none')
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1],
+                c='red', edgecolors='none', s=100)
     plt.show()
     keep_running = input('Make anther walk?(y/n):')
     if keep_running == 'n':
